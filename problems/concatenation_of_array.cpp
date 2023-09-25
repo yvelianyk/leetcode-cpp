29d8260d31ee05fd1deb50660bcaee53ca37a058
yvelianyk <yaroslav.velianyk@gmail.com>
2023-09-25 10:37:38 +0300

    feat[E-1929]: concatenate array
    - use in-place algorithm to save memory

--- problems/concatenation_of_array.cpp
+++ problems/concatenation_of_array.cpp
@@ -4,16 +4,32 @@
 using namespace std;
 
 //  https://leetcode.com/problems/concatenation-of-array/description/
+
+// create new copy of vector
+// class Solution {
+// public:
+//     vector<int> getConcatenation(vector<int>& nums) {
+//         vector<int> res;
+//         int size = nums.size();
+//         for (int i = 0; i < size * 2; i++) {
+//             res.push_back(nums[i % size]);
+//         }
+        
+//         return res;
+//     }
+// };
+
+// in place algorithm:
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-        vector<int> res;
         int size = nums.size();
-        for (int i = 0; i < size * 2; i++) {
-            res.push_back(nums[i % size]);
+        nums.resize(size * 2);
+        for (int i = 0; i < size ; i++) {
+            nums[i + size] = nums[i];
         }
         
-        return res;
+        return nums;
     }
 };
 
@@ -22,4 +38,5 @@ int main()
     Solution s;
     vector<int> array = {1,2,3,4};
     vector<int> res = s.getConcatenation(array);
+    cout << "";
 }
