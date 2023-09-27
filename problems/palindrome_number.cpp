701153d54f6eaf875f9d39a31c06f87cbb0c22f4
yvelianyk <yaroslav.velianyk@gmail.com>
2023-09-27 15:37:17 +0300

    feat[E-9]: is number a palindrom
    - convert number to int list
    - check if palindrom using two pointers technique

---
+++ problems/palindrome_number.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        vector<int> digits = get_digits(x);
+        int start = 0, end = digits.size() - 1;
+
+        while (start < end) {
+            if (digits[start++] != digits[end--]) return false;
+        }
+        
+        return true;
+    }
+
+private:
+    vector<int> get_digits(int x) {
+        vector<int> result;
+        while(x > 0) {
+            result.push_back(x % 10);
+            x = x/10;
+        }
+        return result;
+    }
+};
+
+int main() {
+    Solution s;
+    cout << s.isPalindrome(32);
+}
