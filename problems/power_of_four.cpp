f198711a516b96399e9472e4386b3ee449009bf2
yvelianyk <yaroslav.velianyk@gmail.com>
2023-09-28 16:40:18 +0300

    fix[E342]: add link and output

--- problems/power_of_four.cpp
+++ problems/power_of_four.cpp
@@ -2,11 +2,12 @@
 
 using namespace std;
 
+// https://leetcode.com/problems/power-of-four/
 class Solution {
 public:
     bool isPowerOfFour(int n) {
         if (n > 0 && ((n & (n - 1)) == 0)) {
-            return (n & 0b10101010101010101010101010101010); 
+            return (n & 0b01010101010101010101010101010101); 
         }
         return false;
     }
