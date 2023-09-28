3b7f4e2822aef8150546415b438709ccdf5dec0b
yvelianyk <yaroslav.velianyk@gmail.com>
2023-09-28 15:13:26 +0300

    feat[E-342]: is number a power of four
    - using bit manipulation
    - firstly check if number is a power of two. To do this, you need
    to check if number has only one set bit and it should be leading bit.
    - than you need to check if it's power of four by ensuring set bit
    is in even position

---
+++ problems/power_of_four.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if (n > 0 && ((n & (n - 1)) == 0)) {
+            return (n & 0b10101010101010101010101010101010); 
+        }
+        return false;
+    }
+};
+
+int main() {
+    Solution s;
+    int param = 16;
+    cout << "Is " << param << " power of four?" << std::endl;
+    cout << s.isPowerOfFour(16) << std::endl;
+    cout << "should be true" << std::endl;
+}
