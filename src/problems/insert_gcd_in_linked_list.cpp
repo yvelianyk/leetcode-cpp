4943c3a934b4508c07b2695d71d400d83c80af7a
yvelianyk <yaroslav.velianyk@gmail.com>
2023-10-10 17:16:41 +0300

    feat[M-2807]: insert gcd into linked list

--- src/problems/insert_gcd_in_linked_list.cpp
+++ src/problems/insert_gcd_in_linked_list.cpp
@@ -4,15 +4,15 @@
 class Solution {
 public:
     ListNode *insertGreatestCommonDivisors(ListNode *head) {
-        ListNode *curr = head -> next;
+        ListNode *curr = head->next;
         ListNode *prev = head;
         while (curr != nullptr) {
-                int div = gcd(curr->val, prev ->val);
-                ListNode* gcd_node = new ListNode(div);
-                prev -> next = gcd_node;
-                gcd_node -> next = curr;
-                prev = curr;
-                curr = curr -> next;
+            int div = gcd(curr->val, prev->val);
+            ListNode *gcd_node = new ListNode(div);
+            prev->next = gcd_node;
+            gcd_node->next = curr;
+            prev = curr;
+            curr = curr->next;
         }
         return head;
     }
